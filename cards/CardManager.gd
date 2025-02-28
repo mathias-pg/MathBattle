150ae631db90db3c55e2cb03f2324eba52efae85
mathias.pg <mathias.petibongravier@gmail.com>
2025-02-28 18:43:34 +0100

    Ajout des slots pour mettre les cartes

--- cards/CardManager.gd
+++ cards/CardManager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const COLLISION_MASK_CARD = 1
+const COLLISION_MASK_CARD_SLOT = 2
 var card_being_dragged
 var screen_size
 var is_hovering_on_card
@@ -20,13 +21,23 @@ func _input(event):
 		if event.is_pressed():
 			var card = raycast_check_for_card()
 			if card:
-				card_being_dragged = card
-				card.scale = Vector2(1, 1)
+				start_drag(card)
 		else:
 			if card_being_dragged:
-				card_being_dragged.scale = Vector2(1.05, 1.05)
-				card_being_dragged = null
-			
+				finish_drag()
+
+func start_drag(card):
+	card_being_dragged = card
+	card.scale = Vector2(1, 1)
+	
+func finish_drag():
+	card_being_dragged.scale = Vector2(1.05, 1.05)
+	var card_slot_found = raycast_check_for_card_slot()
+	if card_slot_found and not card_slot_found.card_in_slot:
+		card_being_dragged.position = card_slot_found.position
+		card_being_dragged.get_node("Area2D/CollisionShape2D").disabled = true
+		card_slot_found.card_in_slot = true
+	card_being_dragged = null
 			
 func connect_card_signals(card):
 	card.connect("hovered", on_hovered_over_card)
@@ -54,6 +65,17 @@ func highlight_card(card, hovered):
 		card.scale = Vector2(1, 1)
 		card.z_index = 1
 	
+func raycast_check_for_card_slot():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new()
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = COLLISION_MASK_CARD_SLOT
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null
+	
 func raycast_check_for_card():
 	var space_state = get_world_2d().direct_space_state
 	var parameters = PhysicsPointQueryParameters2D.new()
@@ -62,7 +84,6 @@ func raycast_check_for_card():
 	parameters.collision_mask = COLLISION_MASK_CARD
 	var result = space_state.intersect_point(parameters)
 	if result.size() > 0:
-		# return result[0].collider.get_parent()
 		return get_card_with_highest_z_index(result)
 	return null
 	
