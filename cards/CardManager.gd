7e140d9a489dc601f16975711f567f71c446a902
mathias.pg <mathias.petibongravier@gmail.com>
2025-03-01 12:54:56 +0100

    Ajout du deck qui marche

--- cards/CardManager.gd
+++ cards/CardManager.gd
@@ -5,28 +5,19 @@ const COLLISION_MASK_CARD_SLOT = 2
 var card_being_dragged
 var screen_size
 var is_hovering_on_card
-var player_hand_reference
+var player_hand_reference	
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
 	player_hand_reference = $"../PlayerHand"
+	$"../InputManager".connect("left_mouse_button_released", on_left_click_released)
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if card_being_dragged:
 		var mouse_pos = get_global_mouse_position()
 		card_being_dragged.position = Vector2(clamp(mouse_pos.x, 0, screen_size.x), clamp(mouse_pos.y, 0, screen_size.y))
-
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.is_pressed():
-			var card = raycast_check_for_card()
-			if card:
-				start_drag(card)
-		else:
-			if card_being_dragged:
-				finish_drag()
 
 func start_drag(card):
 	card_being_dragged = card
@@ -43,6 +34,10 @@ func finish_drag():
 	else:
 		player_hand_reference.add_card_to_hand(card_being_dragged)
 	card_being_dragged = null
+	
+func on_left_click_released():
+	if card_being_dragged:
+		finish_drag()
 			
 func connect_card_signals(card):
 	card.connect("hovered", on_hovered_over_card)
---
+++ singlePlayer/Deck.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+const CARD_SCENE_PATH = "res://cards/Card.tscn"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func draw_card():
+	var card_scene = preload(CARD_SCENE_PATH)
+	var new_card = card_scene.instantiate()
+	$"../CardManager".add_child(new_card)
+	new_card.name = "Card"
+	$"../PlayerHand".add_card_to_hand(new_card)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
