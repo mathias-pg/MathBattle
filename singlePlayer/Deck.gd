4fd1c030d05b0d6fc288f280763d12f47f0cef18
mathias.pg <mathias.petibongravier@gmail.com>
2025-03-16 14:54:38 +0100

    limitation du nombre de cartes piochées

--- singlePlayer/Deck.gd
+++ singlePlayer/Deck.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const CARD_SCENE_PATH = "res://Cards/Card.tscn"
 const CARD_DRAW_SPEED = 0.5
+const MAX_CARDS = 5
+
 
 var card_database_reference
 
@@ -10,21 +12,22 @@ func _ready() -> void:
 	card_database_reference = preload("res://Cards/CardDataBase.gd")
 
 func draw_card():
-	var keys = card_database_reference.CARD_TEXTURES.keys()
-	var random_index = randi() % keys.size() 
-	var random_key = keys[random_index]
-	var random_path = card_database_reference.CARD_TEXTURES[random_key]
-	
-	var card_scene = load(CARD_SCENE_PATH)
-	var new_card = card_scene.instantiate()
-	var sprite_node = new_card.get_node("CardImage")
-	sprite_node.texture = load(random_path)
-	$"../CardManager".add_child(new_card)
-	new_card.name = "Card"
-	new_card.card_value = card_database_reference.CARD_VALUES[random_key]
-	new_card.card_sign = card_database_reference.CARD_SIGNS[random_key]
-	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
-	new_card.get_node("AnimationPlayer").play("card_flip")
+	if $"../PlayerHand".player_hand.size() < MAX_CARDS :
+		var keys = card_database_reference.CARD_TEXTURES.keys()
+		var random_index = randi() % keys.size() 
+		var random_key = keys[random_index]
+		var random_path = card_database_reference.CARD_TEXTURES[random_key]
+		
+		var card_scene = load(CARD_SCENE_PATH)
+		var new_card = card_scene.instantiate()
+		var sprite_node = new_card.get_node("CardImage")
+		sprite_node.texture = load(random_path)
+		$"../CardManager".add_child(new_card)
+		new_card.name = "Card"
+		new_card.card_value = card_database_reference.CARD_VALUES[random_key]
+		new_card.card_sign = card_database_reference.CARD_SIGNS[random_key]
+		$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+		new_card.get_node("AnimationPlayer").play("card_flip")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
